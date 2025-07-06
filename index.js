8ba51ff199ea6b6992805ae995b9146ef8f15861
samarth <agarwalsamarth994@gmail.com>
2025-07-07 00:37:19 +0530

    s mistake

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/', firRoutes);
-app.use('./api',sosRoutes);
+app.use('/api/',sosRoutes);
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
