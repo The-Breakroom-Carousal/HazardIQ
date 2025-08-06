9e6a8c5b9aef7016c0b25400a28bf966dce948a2
samarth <agarwalsamarth994@gmail.com>
2025-08-06 20:56:38 +0530

    m

--- initDB.js
+++ initDB.js
@@ -83,7 +83,7 @@ const createSosEventsTable  = async () => {
   try {
     await pool.query(`
       CREATE TABLE IF NOT EXISTS sos_events (
-        id   PRIMARY KEY,
+        id  SERIAL  PRIMARY KEY,
         firebase_uid TEXT NOT NULL,
         type TEXT NOT NULL,
         latitude DOUBLE PRECISION NOT NULL,
